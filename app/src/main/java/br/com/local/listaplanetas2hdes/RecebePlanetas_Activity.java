4cab6bdde2790effc17bf325c2b9a2ce38012dda
Laercio-pc <lndsilva@hotmail.com>
2020-06-17 22:30:25 -0300

    seta home

--- app/src/main/java/br/com/local/listaplanetas2hdes/RecebePlanetas_Activity.java
+++ app/src/main/java/br/com/local/listaplanetas2hdes/RecebePlanetas_Activity.java
@@ -1,9 +1,11 @@
 package br.com.local.listaplanetas2hdes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,13 +28,29 @@ public class RecebePlanetas_Activity extends AppCompatActivity {
         //Declarar as variáveis para receber os valores do mesmo tipo
 
         String nome = intent.getStringExtra("nome_planeta");
-        int imagem = intent.getIntExtra("imagem_planeta",0);
+        int imagem = intent.getIntExtra("imagem_planeta", 0);
 
         //os objetos recebendo os valores
         txtRecebeNomePlanetas.setText(nome);
         imgRecebePlanetas.setImageResource(imagem);
 
-        
+        //inserindo a seta na appBar da activity
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    //Para fazer o clique da seta home voltar para a outra activity
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        super.onBackPressed();
     }
 }
